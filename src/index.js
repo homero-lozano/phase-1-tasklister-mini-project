45dcd7de1dc02253fc2bc88aa206e56b07fca0fb
Nico Lozano <homeronicolaslozano@gmail.com>
2024-04-19 14:04:51 -0400

    Challenge: TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.querySelector('#create-task-form')
+const taskInput = document.querySelector('#new-task-description')
+const taskList = document.querySelector('#tasks') 
+
+form.addEventListener('submit', event => {
+  event.preventDefault(); 
+  const taskDescription = taskInput.value;
+  const taskItem = document.createElement('li') 
+  taskItem.textContent = taskDescription;
+  const deleteButton = document.createElement('button')
+  deleteButton.textContent = 'x' 
+  deleteButton.addEventListener('click', () => {
+    taskItem.remove()
+  })
+  taskItem.appendChild(deleteButton)
+  taskList.appendChild(taskItem)
+  taskInput.value = ''
+
+   })
+})
+
